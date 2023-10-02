064e00ce9b94479d33cb02e6b825307443f01eef
Alisa Pokormlyak <pokormlyakao@factory5.ai>
2023-10-02 11:27:03 +0300

    resolve thread

--- homework_02/base.py
+++ homework_02/base.py
@@ -7,11 +7,11 @@ LOGGER = logging.getLogger(__name__)
 
 
 class Vehicle(ABC):
-    def __init__(self, weight, fuel, fuel_consumption):
-        self.weight = weight | 0
+    def __init__(self, weight=0, fuel=0, fuel_consumption=0):
+        self.weight = weight
         self.started = False
-        self.fuel = fuel | 0
-        self.fuel_consumption = fuel_consumption | 0
+        self.fuel = fuel
+        self.fuel_consumption = fuel_consumption
 
     def start(self):
         if self.started is False:
